7905da584ea50863fb5776851929e733656eacae
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2014-10-12 17:15:30 +0200

    result file parsing improved

--- src/toolchain/lib/input/resultfile.py
+++ src/toolchain/lib/input/resultfile.py
@@ -2,25 +2,19 @@ import re
 from sympy import Symbol
 from sympy.polys import Poly
 from data.constraint import Constraint
+from data.rationalfunction import RationalFunction
 
 
 def read_result_file(input_path):
     inputfile = open(input_path)
     inputstring = inputfile.read()
     inputfile.close()
-    print(inputstring)
-
-    
-
+   
     parameters = re.findall('!Parameters:\s(.*)', inputstring)[0].split(", ")
-    print(parameters)
-    
     
     match = re.search('!Result:\s*((\w|[)(\+\*\^-])*?)(\s?/\s?((\w|[)(\+\*\^-])*?))?\n', inputstring)
     resultingRatFunNom = match.group(1)
     resultingRatFunDen = match.group(4)
-    print(resultingRatFunNom)
-    print(resultingRatFunDen)
     
     welldefined_constraintsString = re.findall(r'(!Well-formed Constraints:\s*\n.+?)(?=!|(?:\s*\Z))', inputstring, re.DOTALL)[0]
     welldefined_constraintsStrings = welldefined_constraintsString.split("\n")[:-1]
@@ -34,12 +28,14 @@ def read_result_file(input_path):
 def parse_result_file(path):
     [parameter_strings, welldefined_constraint_strings, graph_preserving_constraint_strings, nominator_string, denominator_string] = read_result_file(path)
     parameters = [ Symbol(name.rstrip()) for name in parameter_strings ]
-    wdconstraints = [ Constraint.__from_str__(cond, parameters) for cond in welldefined_constraint_strings ]
-    print(str(wdconstraints))
-    gpconstraints = [ Constraint.__from_str__(cond, parameters) for cond in graph_preserving_constraint_strings ]
-    print(str(gpconstraints))
+    wdconstraints = [ Constraint.__from_str__(cond, parameters) for cond in welldefined_constraint_strings[1:] ]
+    gpconstraints = [ Constraint.__from_str__(cond, parameters) for cond in graph_preserving_constraint_strings[1:] ]
     nominator = Poly(nominator_string, parameters)
     denominator = Poly(1, parameters)
     if denominator_string != None:
         denominator = Poly(denominator_string, parameters)
-    return [parameters, wdconstraints, gpconstraints, nominator, denominator]  
+        
+    return [parameters, wdconstraints, gpconstraints, RationalFunction(nominator, denominator)] 
+
+def write_result_file(parameters, wdconstraints, gpconstraints, rationalfunction, path):
+    pass
