e348fafdec2f3436afa1f77f572fd8bd14312105
Matthias Volk <matthias.volk@rwth-aachen.de>
2015-01-27 15:31:37 +0100

    Fixed introduced bug in sampling

--- src/toolchain/lib/sampling.py
+++ src/toolchain/lib/sampling.py
@@ -81,7 +81,7 @@ def _distance(p1, p2):
     return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
 
 def filter_sampling(samples, threshold):
-    return {pt : val for (pt, val) in samples.items() if abs(threshold - val) > 0.1}
+    return {pt : val for (pt, val) in samples.items() if abs(threshold - val) <= 0.1}
 
 def near_sampling(samples, threshold, rectangles, limit = 0.1, added_dist = 0.05):
     pass
