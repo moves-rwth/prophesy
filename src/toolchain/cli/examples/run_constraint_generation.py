b0e858d1b21506077a31107de29badeeacf10cf2
Matthias Volk <mvolk@amphitryon.informatik.rwth-aachen.de>
2015-02-07 03:44:48 +0100

    Changed script

--- src/toolchain/cli/examples/run_constraint_generation.py
+++ src/toolchain/cli/examples/run_constraint_generation.py
@@ -11,44 +11,44 @@ target_file = "constraint_generation_{}".format(current_time)
 
 
 benchmarks = [ 
-    ("brp/brp_16-2"),
-    ("crowds/crowds_3-5"),
-    ("nand/nand_10-1"),
-    ("crowds/crowds_5-5"),
-    ("crowds/crowds_10-5"),
-    ("nand/nand_10-2"),
-    ("nand-reward/nand_10-2"),
-    ("crowds/crowds_15-5"),
-    ("nand/nand_10-5"),
-    ("nand-reward/nand_10-5"),
-    ("crowds/crowds_20-5"),
-    ("nand/nand_20-2"),
-    ("nand-reward/nand_20-2"),
-#   ("nand/nand_20-5"),
-#   ("nand-reward/nand_20-5"),
-#   ("brp/brp_128-2"),
-#   ("brp/brp_128-5"),
-#   ("brp/brp_256-2"),
-#   ("brp/brp_256-5"),
+    ("brp/", "brp_16-2"),
+    ("crowds/", "crowds_3-5"),
+    ("nand/", "nand_10-1"),
+    ("crowds/", "crowds_5-5"),
+    ("crowds/", "crowds_10-5"),
+    ("nand/", "nand_10-2"),
+    ("nand-reward/", "nand_10-2"),
+    ("crowds/", "crowds_15-5"),
+    ("nand/", "nand_10-5"),
+    ("nand-reward/", "nand_10-5"),
+    ("crowds/", "crowds_20-5"),
+    ("nand/", "nand_20-2"),
+    ("nand-reward/", "nand_20-2"),
+#   ("nand/", "nand_20-5"),
+#   ("nand-reward/", "nand_20-5"),
+#   ("brp/", "brp_128-2"),
+#   ("brp/", "brp_128-5"),
+#   ("brp/", "brp_256-2"),
+#   ("brp/", "brp_256-5"),
 ]
 
 def runBenchmarks():
-    for (benchmark) in benchmarks:
+    for (name, benchmark) in benchmarks:
         for algorithm in ["rectangles", "quads"]:
-	    target_file = "{}_{}_{}.out".format(target_file, benchmark, algorithm)
+            file_output = "{}_{}_{}.out".format(target_file, benchmark, algorithm)
             command = ["python3",
                         "../buildconstraints.py", 
                         "--rat-file",
-                        "{}.rat".format(benchmark), 
+                        "{}{}.rat".format(name, benchmark),
                         "--samples-file",
-                        "{}.samples".format(benchmark),
+                        "{}{}.samples".format(name, benchmark),
                         "--z3",
                         "z3",
                         END_CRITERIA,
                         str(END_CRITERIA_VALUE),
                         "--{}".format(algorithm),
                         ]
-            output = open(target_file, "a+")
+            output = open(file_output, "a+")
             print("Running buildconstraints on {} ...".format(benchmark))
             output.write(" ".join(command))
             subprocess.call(command, stdout=output);
