e8c4474dd2fcd3166b829fce024573d6a05a1b5d
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2015-09-22 17:14:31 +0200

    added which to find programs in environment

--- src/toolchain/prophesy/util.py
+++ src/toolchain/prophesy/util.py
@@ -33,3 +33,25 @@ def run_tool(args, quiet = False):
             if output != "":
                 print("\t * " + output)
 
+
+def which(program):
+    """
+    See http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python/377028#377028
+    :param program: String with name of the program
+    :return:
+    """
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep) + configuration.get(config.DIRECTORIES, "custom_path").split(","):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
