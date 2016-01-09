98e04b0226ad447f5f6413c1cc08504050700007
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-09 11:25:30 +0100

    setup updates

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 import importlib
 from prophesy.write_config import write_initial_config
 import os
+import sys
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
+
 
 def package_installed(package):
     spec = importlib.util.find_spec(package)
@@ -11,11 +25,13 @@ def do_setup():
     write_initial_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), "prophesy/prophesy.cfg"))
 
     setup(
+        cmdclass = {'test': Tox},
         name="Prophesy",
         version="1.1",
         description="Prophesy - Parametric Probabilistic Model Checking",
         packages=find_packages(),
-        install_requires=['tornado', 'pycket', 'redis', 'sympy', 'shapely', 'numpy', 'matplotlib'],
+        install_requires=['tornado', 'pycket', 'sympy', 'shapely', 'numpy', 'matplotlib'],
+        test_requires=['tox'],
         extras_require = {
             'carl': ["pycarl"],
             'stormpy' : ["stormpy"],
