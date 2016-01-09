a0e4ea63b4eabcc03cc0e9b42b615802d6fbab74
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-09 16:22:42 +0100

    Slightly improved pctlfile

--- prophesy/input/pctlfile.py
+++ prophesy/input/pctlfile.py
@@ -1,9 +1,13 @@
+from util import check_filepath_for_reading
+
 class PctlFile:
     def __init__(self, location):
+        check_filepath_for_reading(location)
         self.location = location
         self.formulas = []
+        self._parse()
 
-    def parse(self):
+    def _parse(self):
         with open(self.location, 'r') as f:
             for line in f:
                 if line.startswith('#') or line.strip() == "":
@@ -12,6 +16,8 @@ class PctlFile:
                     self.formulas.append(line.strip)
 
     def get(self, index):
+        if index >= self.get_nr_formulas():
+            raise IndexError("The PCTL file only contains {0} formulas, index {1} is too large".format(self.get_nr_formulas(), index))
         return self.formulas[index]
 
     def get_nr_formulas(self):
