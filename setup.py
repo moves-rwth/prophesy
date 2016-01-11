da5bd89dd6ee772dc91cb047620b6df9f7d6094d
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-11 16:28:55 +0100

    Better setup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
-import importlib
 from prophesy.write_config import write_initial_config
 import os
 import sys
@@ -17,20 +16,14 @@ class Tox(TestCommand):
         sys.exit(errcode)
 
 
-def package_installed(package):
-    spec = importlib.util.find_spec(package)
-    return spec is not None
-
 def do_setup():
-    write_initial_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), "prophesy/prophesy.cfg"))
-
     setup(
         cmdclass = {'test': Tox},
         name="Prophesy",
         version="1.1",
         description="Prophesy - Parametric Probabilistic Model Checking",
         packages=find_packages(),
-        install_requires=['tornado', 'pycket', 'sympy', 'shapely', 'numpy', 'matplotlib'],
+        install_requires=['tornado', 'pycket', 'redis', 'sympy', 'shapely', 'numpy', 'matplotlib'],
         test_requires=['tox'],
         extras_require = {
             'carl': ["pycarl"],
@@ -40,9 +33,8 @@ def do_setup():
         package_data={'prophesy': ['prophesy.cfg']}
     )
 
-    print("Found pycarl: ", package_installed("pycarl"))
-    print("Found stormpy: ", package_installed("stormpy"))
-    print("Found PyPDF2: ", package_installed("PyPDF2"))
+    write_initial_config(os.path.join(os.path.dirname(os.path.realpath(__file__)), "prophesy/prophesy.cfg"))
+
 
 if __name__ == "__main__":
       do_setup()
