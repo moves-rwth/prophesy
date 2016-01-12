f7671c356ac4bbfb70242b960ba21a6d934aab02
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-12 16:09:21 +0100

    Use the improved MC wrapping

--- prophesy/sampling/sampler_prism.py
+++ prophesy/sampling/sampler_prism.py
@@ -4,19 +4,17 @@ import data.range
 
 class McSampling(Sampler):
     """Perform sampling based on the PRISM input file"""
-    def __init__(self, tool, prism_file, pctl_file):
+    def __init__(self, tool):
         super().__init__()
 
         self.tool = tool
-        self.prism_file = prism_file
-        self.pctl_file = pctl_file
 
     def perform_uniform_sampling(self, intervals, samples_per_dimension):
         assert samples_per_dimension > 1
         ranges = [data.range.create_range_from_interval(interval, samples_per_dimension) for interval in intervals]
-        samples = self.tool.uniform_sample_pctl_formula(self.prism_file, self.pctl_file, ranges)
+        samples = self.tool.uniform_sample(ranges)
         return OrderedDict(samples.items())
 
     def perform_sampling(self, sample_points):
-        samples = self.tool.sample_pctl_formula(self.prism_file, self.pctl_file, sample_points)
+        samples = self.tool.sample(self.prism_file, self.pctl_file, sample_points)
         return OrderedDict(samples.items())
