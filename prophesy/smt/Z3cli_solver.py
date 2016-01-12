6595b6ef15abbde06d44e702de6076845f6ac8b1
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-12 15:51:29 +0100

    Wrapper for Z3 CLI

---
+++ prophesy/smt/Z3cli_solver.py
@@ -0,0 +1,10 @@
+import config
+from config import configuration
+from smt.smtlib import SmtlibSolver
+
+class Z3CliSolver(SmtlibSolver):
+    def __init__(self, location=configuration.get(config.EXTERNAL_TOOLS, "z3"), memout=4000, timeout=100):
+        super(Z3CliSolver, self).__init__(location, memout, timeout)
+
+    def name(self):
+        return "Z3 cli tool"
