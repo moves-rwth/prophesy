91abf3c21871eb765256bf589f2cbf5bda03eab7
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-19 10:17:21 +0100

    added an initial test

---
+++ tests/test_range.py
@@ -0,0 +1,8 @@
+from prophesy.data.range import Range, create_cartisean_product
+
+def test_cartesian_product():
+    r1 = Range(1,3,1)
+    r2 = Range(4,6,1)
+    result = create_cartisean_product([r1, r2])
+    assert len(list(result)) == 9
+
