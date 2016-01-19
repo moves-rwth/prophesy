3b3107d7205975c904876e076643619f3e8d1036
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2016-01-19 10:16:56 +0100

    latest version of stormpy

--- prophesy/modelcheckers/stormpy.py
+++ prophesy/modelcheckers/stormpy.py
@@ -1,12 +1,19 @@
-
-import stormpy
 import stormpy.info
+import stormpy.core
 
-from modelcheckers.ppmc  import ParametricProbabilisticModelChecker
+from modelcheckers.ppmc import ParametricProbabilisticModelChecker
+from modelcheckers.pmc import BisimulationType
+
+from exceptions.not_enough_information_error import NotEnoughInformationError
+
 
 class StormpyModelChecker(ParametricProbabilisticModelChecker):
     def __init__(self):
         self.bisimulation = BisimulationType.strong
+        self.pctl_formula = None
+        self.prism_file = None
+        self.program = None
+        stormpy.core.setUp()
 
     def name(self):
         return "stormpy"
@@ -15,9 +22,15 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
         return stormpy.info.Version.short()
 
 
-    def set_pctl_formula(self, formula): raise NotImplementedError
+    def set_pctl_formula(self, formula):
+        if self.program == None:
+            raise NotEnoughInformationError("Stormpy requires the program before the formula can be loaded.")
+        self.pctl_formula = stormpy.core.parseFormulae(formula, self.program)
 
-    def load_model_from_prismfile(self, prismfile): raise NotImplementedError
+    def load_model_from_prismfile(self, prismfile):
+        self.prism_file = prismfile
+        self.program = stormpy.core.parseProgram(prismfile.location)
+
 
     def set_bisimulation(self, BisimulationType): raise NotImplementedError
 
@@ -25,4 +38,14 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
 
     def sample(self, samplePoints): raise NotImplementedError
 
-    def get_rational_function(self): raise NotImplementedError
+    def get_rational_function(self):
+        model = None
+        if self.prism_file.nr_parameters() == 0:
+            model = stormpy.core.buildModelFromPrismProgram(self.program, self.pctl_formula)
+        else:
+            model = stormpy.core.buildModel(self.program, self.pctl_formula[0])
+            print(type(model))
+            print(model.model_type)
+            model = model.asPdtmc()
+        stormpy.core.performStateElimination(model, self.pctl_formula[0])
+
