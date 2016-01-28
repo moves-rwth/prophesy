dc94e59df24c70091b2f85ba3319fdafdc15e43b
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2016-01-28 10:21:46 +0100

    Getting all items from dictionary should work now

--- prophesy/config.py
+++ prophesy/config.py
@@ -29,15 +29,13 @@ class Configuration():
     def getAll(self):
         if self._importedFrom == "":
             self._importFromFile()
-        result = "{"
+
+        # Convert configuration into dict of dicts
+        # where each section has its own dictionary with (key,value)
+        result = {}
         sections = self._config.sections()
         for section in sections:
-            result = result + section + ':"{'
-            for key in self._config[section]:
-                value = self._config[section][key]
-                result = result + key + ":" + value + ", "
-            result = result + '"}, '
-        result = result + "}"
+             result[section] = dict(self._config.items(section))
         return result
 
     def set(self, section, key, value):
