73664f13e8e0bc0c204e6c50c70936dac5c97b99
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2016-04-20 18:56:54 +0200

    Test case for setting of config values

--- tests/gui/test_tornado.py
+++ tests/gui/test_tornado.py
@@ -1,5 +1,9 @@
 from tornado.testing import AsyncHTTPTestCase
+from tornado.httpclient import HTTPRequest
+import urllib
 from prophesy.webservice.webcegar import make_app
+import json
+import random
 
 class TestTornado(AsyncHTTPTestCase):
     """ Testing of the tornado web framework. """
@@ -8,6 +12,11 @@ class TestTornado(AsyncHTTPTestCase):
         """ Override to return own application. """
         return make_app("localhost")
 
+    def setUp(self):
+        super().setUp()
+        response = self.fetch('/results')
+        self.cookies = response.headers["Set-Cookie"]
+
     def test_homepage(self):
         """ Test the prophesy homepage. """
         response = self.fetch('/')
@@ -15,7 +24,27 @@ class TestTornado(AsyncHTTPTestCase):
     
     def test_config(self):
         """ Test the configuration. """
-        response = self.fetch('/config')
+        new_value = str(random.random())
+        response = self.fetch('/config/test/test_value')
+        s = response.body.decode('UTF-8')
+        value_before = json.loads(s)["data"]
+        while new_value == value_before:       
+            new_value = str(random.random())
+ 
+        # Change value
+        headers_new = {"Host": "localhost:4242",
+            "Accept": "*/*",
+            "Content-Type": "application/json",
+            "Cookie": self.cookies,
+        }
+        response = self.fetch('/config/test/test_value?data=' + new_value, method='POST', headers = headers_new, body="")
         self.assertEqual(response.code, 200)
-        self.assertIn(response.body, "sampling_threshold_new")
+        s = response.body.decode('UTF-8')
+        
+        # Change new value
+        response = self.fetch('/config/test/test_value')
+        s = response.body.decode('UTF-8')
+        value_after = json.loads(s)["data"]
+        self.assertNotEqual(value_before, value_after)
+        self.assertEqual(value_after, new_value)
 
