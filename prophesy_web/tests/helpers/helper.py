76ffeb615fec5daed249ee34364a6735c2648946
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-07-18 12:39:59 +0200

    Moved gui test into prophesy_web dir

---
+++ prophesy_web/tests/helpers/helper.py
@@ -0,0 +1,7 @@
+import os
+
+example_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir, "benchmarkfiles"))
+
+
+def get_example_path(*paths):
+    return os.path.join(example_dir, *paths)
