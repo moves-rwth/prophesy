78e0ec8acdf8e27ae90ddcee9f3d3468358bbb99
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-07-27 16:45:27 +0200

    Documentation for plot

--- prophesy/output/plot.py
+++ prophesy/output/plot.py
@@ -2,7 +2,8 @@ import logging
 import tempfile
 
 import matplotlib
-matplotlib.use('Agg') # use for plotting without X-server
+
+matplotlib.use('Agg')  # use for plotting without X-server
 from matplotlib import pyplot
 from matplotlib import patches
 from matplotlib.colors import ColorConverter
@@ -12,23 +13,32 @@ from shapely.geometry.polygon import Polygon
 
 import numpy as np
 
-from prophesy.data.hyperrectangle import  HyperRectangle
+from prophesy.data.hyperrectangle import HyperRectangle
 from prophesy.config import configuration
-
 
 logger = logging.getLogger(__name__)
 
 
 def plot_samples(samples, parameters, safe_above_threshold, threshold):
-    """Plot samples and return path to file."""
+    """
+    Create pdf file displaying the samples.
+    :param samples: List of samples.
+    :param parameters: Paramaters.
+    :param safe_above_threshold: If true, all values above the threshold are considered
+                                 safe (indicated by green color), otherwise they are considered
+                                unsafe (red color).
+    :param threshold: Threshold for safe/unsafe values.
+    :return: Path to pdf file containing the samples plot.
+    """
     if not safe_above_threshold:
         Plot.flip_green_red = True
 
     _, plot_path = tempfile.mkstemp(suffix=".pdf", prefix="sampling_", dir=configuration.get_plots_dir())
 
-    samples_green = [res.instantiation.get_point(parameters) for res in samples.instantiation_results() if res.result >= threshold]
+    samples_green = [res.instantiation.get_point(parameters) for res in samples.instantiation_results() if
+                     res.result >= threshold]
     samples_red = [res.instantiation.get_point(parameters) for res in samples.instantiation_results()
-                     if res.result < threshold]
+                   if res.result < threshold]
 
     Plot.plot_results(parameters=parameters, samples_green=samples_green, samples_red=samples_red,
                       path_to_save=plot_path, display=False)
@@ -38,10 +48,21 @@ def plot_samples(samples, parameters, safe_above_threshold, threshold):
 
 
 class Plot(object):
+    """
+    Class handling plotting of samples and result boxes.
+    """
+
     flip_green_red = False
 
     @staticmethod
     def plot_poly(subplot, poly, *args, **kwargs):
+        """
+        Plot a polygon or hyperrectangle.
+        :param subplot:
+        :param poly: Polygon.
+        :param args:
+        :param kwargs:
+        """
         if isinstance(poly, HyperRectangle):
             verts = poly.np_vertices()
             tmp = np.array(verts[3])
@@ -54,7 +75,7 @@ class Plot(object):
 
         if isinstance(poly, Polygon):
             poly = poly.exterior
-            
+
         # If hatched, set edge to black regardless of given argument
         if poly.__class__ != LineString and 'hatch' in kwargs:
             kwargs['ec'] = 'none'
@@ -72,9 +93,25 @@ class Plot(object):
     @staticmethod
     def plot_results(parameters,
                      samples_green=[], samples_red=[], samples_blue=[],
-                     poly_green=[], poly_red=[], poly_blue_crossed=[], poly_blue_dotted = [], poly_blue=[],
+                     poly_green=[], poly_red=[], poly_blue_crossed=[], poly_blue_dotted=[], poly_blue=[],
                      anchor_points=[], additional_arrows=[],
                      path_to_save=None, display=False):
+        """
+        Plot all results.
+        :param parameters: Parameters.
+        :param samples_green: Samples with color green.
+        :param samples_red: Samples with color red.
+        :param samples_blue: Samples with color blue.
+        :param poly_green: Polygons with color green.
+        :param poly_red: Polygons with color red.
+        :param poly_blue_crossed: Polygons with color blue and crossed hatch.
+        :param poly_blue_dotted: Polygons with color blue and dotted hatch.
+        :param poly_blue: Polygons with color blue.
+        :param anchor_points: List of anchor points with direction.
+        :param additional_arrows: List of additional arrows.
+        :param path_to_save: Paths for pdf file or None if no file should be generated.
+        :param display: If true, the plot will be displayed automatically.
+        """
         logger.info("Plot results")
 
         if len(parameters) > 2:
@@ -114,14 +151,15 @@ class Plot(object):
                 d = 0.02
                 dx = d if anchor.dir.value[0] else -d
                 dy = d if anchor.dir.value[1] else -d
-                ax1.arrow(anchor.pos.x, anchor.pos.y, dx, dy, head_width=d/2, head_length=d/2, color='blue')
+                ax1.arrow(anchor.pos.x, anchor.pos.y, dx, dy, head_width=d / 2, head_length=d / 2, color='blue')
 
             colorc = ColorConverter()
             for line in additional_arrows:
                 line_points = list(line.coords)
                 point1 = line_points[0]
                 point2 = line_points[1]
-                ax1.arrow(point1[0], point1[1], point2[0] - point1[0], point2[1] - point1[1], head_width=0.01, head_length=0.01, color='gray')
+                ax1.arrow(point1[0], point1[1], point2[0] - point1[0], point2[1] - point1[1], head_width=0.01,
+                          head_length=0.01, color='gray')
 
             for box in poly_green:
                 Plot.plot_poly(ax1, box, fc=colorc.to_rgba("#4aa02c", 0.6), ec=colorc.to_rgba("#4aa02c"), hatch="o")
