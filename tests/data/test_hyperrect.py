f1e2178bc6e11495344ab02ed233c81588513a5f
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-08-04 18:19:23 +0200

    merged files containing tests about hyperrectangles

--- tests/data/test_hyperrect.py
+++ tests/data/test_hyperrect.py
@@ -3,6 +3,32 @@ from prophesy.data.hyperrectangle import HyperRectangle
 import prophesy.adapter.pycarl as pc
 
 
+
+def test_hyperrectangle_splitting():
+    i = Interval(0.0, BoundType.closed, 2.0, BoundType.closed)
+    j = Interval(4.0, BoundType.open, 8.0, BoundType.closed)
+    r = HyperRectangle(i,j)
+    res = r.split_in_every_dimension()
+    i00 = HyperRectangle(Interval(0.0, BoundType.closed, 1.0, BoundType.open), Interval(4.0, BoundType.open, 6.0, BoundType.open))
+    i01 = HyperRectangle(Interval(0.0, BoundType.closed, 1.0, BoundType.open), Interval(6.0, BoundType.closed, 8.0, BoundType.closed))
+    assert i00 in res
+    assert i01 in res
+    i00 = HyperRectangle(Interval(1.0, BoundType.closed, 2.0, BoundType.closed), Interval(4.0, BoundType.open, 6.0, BoundType.open))
+    i01 = HyperRectangle(Interval(1.0, BoundType.closed, 2.0, BoundType.closed), Interval(6.0, BoundType.closed, 8.0, BoundType.closed))
+    assert i00 in res
+    assert i01 in res
+
+
+def test_hyperrectangle_vertices():
+    i = Interval(0.0, BoundType.closed, 2.0, BoundType.closed)
+    j = Interval(4.0, BoundType.closed, 8.0, BoundType.closed)
+    r = HyperRectangle(i,j)
+    vertices  = r.vertices()
+    assert len(vertices) == 4
+    assert Point(0.0, 4.0) in vertices
+    assert Point(2.0, 4.0) in vertices
+
+
 def test_get_middle_component():
     h1 = HyperRectangle(string_to_interval("[-10,10]",float))
     h2 = HyperRectangle(string_to_interval("[-5,5]", float))
