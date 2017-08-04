14c8c347903a331244f2eb39bf582e970f7bed85
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-08-04 17:07:19 +0200

    fix silly mistake

--- prophesy/data/nice_approximation.py
+++ prophesy/data/nice_approximation.py
@@ -2,8 +2,8 @@ from prophesy.adapter.pycarl import Rational, Integer
 
 
 class FixedDenomFloatApproximation:
-    def __init__(self, Integer(denom)):
-        self.denom = denom
+    def __init__(self, denom):
+        self.denom = Integer(denom)
 
     def find(self, input):
         return Rational(Integer(int(float(input)*float(self.denom))), self.denom)
