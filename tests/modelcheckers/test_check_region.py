c07adc29b0c3e7099ce5a21e6edbcab3abed7eec
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-08-08 17:49:22 +0200

    Test for checking region

---
+++ tests/modelcheckers/test_check_region.py
@@ -0,0 +1,36 @@
+from prophesy.modelcheckers.storm import StormModelChecker
+from prophesy.modelcheckers.stormpy import StormpyModelChecker
+from prophesy.modelcheckers.prism import PrismModelChecker
+from prophesy.input.prismfile import PrismFile
+from prophesy.input.pctlfile import PctlFile
+from prophesy.data.hyperrectangle import HyperRectangle
+from prophesy.regions.region_checker import RegionCheckResult
+
+from helpers.helper import get_example_path
+from requires import *
+
+tools = [
+    require_storm()(StormModelChecker),
+    # require_prism(rational_function=True)(PrismModelChecker),
+    # require_stormpy()(StormpyModelChecker),
+]
+
+
+@pytest.mark.parametrize("MCType", tools)
+def test_check_hyperrectangle(MCType):
+    tool = MCType()
+    prism_file = PrismFile(get_example_path("pdtmc", "brp", "brp_16-2.pm"))
+    pctl_file = PctlFile(get_example_path("pdtmc", "brp", "property1.pctl"))
+    tool.load_model_from_prismfile(prism_file)
+    tool.set_pctl_formula(pctl_file.get(0))
+
+    hyperrectangle = HyperRectangle.from_region_string("0.25<=pL<=0.5,0.25<=pK<=0.5", prism_file.parameters)
+    result = tool.check_hyperrectangle(prism_file.parameters, hyperrectangle, 0.7, True)
+    assert len(result) == 1
+    for res, region in result:
+        assert res == RegionCheckResult.Satisfied
+
+    hyperrectangle = HyperRectangle.from_region_string("0.5<=pL<=0.7,0.5<=pK<=0.7", prism_file.parameters)
+    result = tool.check_hyperrectangle(prism_file.parameters, hyperrectangle, 0.7, True)
+    for res, region in result:
+        assert res == RegionCheckResult.Satisfied
