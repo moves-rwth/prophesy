453265ad167ad88545c97b1b70398b1406dabe8a
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-08-08 14:02:49 +0200

    clean config.py

--- prophesy/smt/smtlib.py
+++ prophesy/smt/smtlib.py
@@ -2,9 +2,8 @@ import subprocess
 import functools
 import logging
 
-from prophesy.config import TOOLNAME
 from prophesy.smt.smt import SMTSolver, Answer, VariableDomain
-from prophesy.adapter.pycarl import Constraint, Formula, Rational
+from prophesy.adapter.pycarl import Rational
 
 logger = logging.getLogger(__name__)
 
@@ -13,9 +12,9 @@ def _smtfile_header():
     formula = "(set-logic QF_NRA)\n"
     formula += "(set-info :source |\n"
     formula += "Probabilistic verification" + "\n"
-    formula += TOOLNAME + "\n"
+    formula +=  "prophesy" + "\n"
     formula += "|)\n"
-    formula += "(set-info :smt-prophesy-version 2.0)\n"
+    formula += "(set-info :smt-version 2.0)\n"
     formula += "(set-info :category \"industrial\")\n"
     return formula
 
