89deebb6077730cd77b2593d2e77eb0f5774dbfa
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-08-09 18:04:54 +0200

    Raise exception if stormpy is missing module pars

--- prophesy/adapter/stormpy.py
+++ prophesy/adapter/stormpy.py
@@ -7,4 +7,7 @@ if configuration.is_module_available("stormpy"):
     from stormpy import *
     import stormpy.info as info
     import stormpy.logic as logic
+
+    if not stormpy._config.storm_with_pars:
+        raise ImportError("Stormpy was built without necessary parametric module.")
     import stormpy.pars as pars
