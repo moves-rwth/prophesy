c47a17157399b6b0ecaad2074a80f4a1ba4ef323
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-08-10 14:54:05 +0200

    Store stormpy objects for incremental calls

--- prophesy/modelcheckers/stormpy.py
+++ prophesy/modelcheckers/stormpy.py
@@ -6,7 +6,6 @@ from prophesy.modelcheckers.ppmc import ParametricProbabilisticModelChecker
 from prophesy.modelcheckers.pmc import BisimulationType
 from prophesy.exceptions.configuration_error import ConfigurationError
 from prophesy.exceptions.not_enough_information_error import NotEnoughInformationError
-from prophesy.data.property import OperatorBound
 from prophesy.input.solutionfunctionfile import ParametricResult
 from prophesy.data.constant import Constants
 from prophesy.data.samples import InstantiationResultDict, InstantiationResult
@@ -31,15 +30,19 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
             raise ModuleError(
                 "Module stormpy is needed for using the Python API for Storm. Maybe your config is outdated?")
 
-        self.bisimulation = stormpy.BisimulationType.STRONG
-        self.pctlformula = None
         self.prismfile = None
+        self.pctlformula = None
         self.constants = None
-        self.program = None
-        self.last_result = None
-        self.model = None
-        self.parameter_constraints = None
-        self.graph_preservation_constraints = None
+        self.bisimulation = stormpy.BisimulationType.STRONG
+        # Storing objects of stormpy for incremental calls
+        self._program = None
+        self._model = None
+        self._parameter_constraints = None
+        self._graph_preservation_constraints = None
+        self._parameter_mapping = None
+        self._model_instantiator = None
+        self._pla_checker = None
+        self._pla_formula = None
 
     def name(self):
         return "stormpy"
@@ -48,85 +51,98 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
         return str(stormpy.info.Version.short)
 
     def set_pctl_formula(self, formula):
-        # TODO instead of going via string, do a bit more meaningful stuff.
-        if self.program is None:
+        if self._program is None:
             self.pctlformula = stormpy.parse_properties(str(formula))
         else:
-            self.pctlformula = stormpy.parse_properties_for_prism_program(str(formula), self.program)
+            self.pctlformula = stormpy.parse_properties_for_prism_program(str(formula), self._program)
+        # Reset formula for PLA
+        self._pla_formula = None
 
     def load_model_from_prismfile(self, prism_file, constants=Constants()):
         self.prismfile = prism_file
         self.constants = constants
-        # TODO use constants here.
-        self.program = stormpy.parse_prism_program(self.prismfile.location)
+        self._program = stormpy.parse_prism_program(self.prismfile.location)
+        # Reset stormpy objects
+        self._model = None
+        self._parameter_constraints = None
+        self._graph_preservation_constraints = None
+        self._parameter_mapping = None
+        self._model_instantiator = None
+        self._pla_checker = None
 
-    def set_bisimulation_type(self, bisimulationType):
-        if bisimulationType == BisimulationType.weak:
-            self.bisimulation = stormpy.BisimulationType.WEAK
-        elif bisimulationType == BisimulationType.strong:
-            self.bisimulation = stormpy.BisimulationType.STRONG
-        elif bisimulationType == BisimulationType.none:
-            self.bisimulation == None
-        else:
-            raise ConfigurationError("Bisimulation type {} not valid.".format(bisimulationType))
-
-    def build_model(self):
-        """
-        Build prism model and (optionally) perform bisimulation.
-        """
-        logger.info("Build model")
-
-        if self.prismfile is None:
-            raise NotEnoughInformationError("Prism model not set.")
-        if self.pctlformula is None:
-            raise NotEnoughInformationError("PCTL formula not set.")
-
-        if not self.program.undefined_constants_are_graph_preserving:
+        if not self._program.undefined_constants_are_graph_preserving:
             # Need to instantiate constants
-            description = stormpy.SymbolicModelDescription(self.program)
+            description = stormpy.SymbolicModelDescription(self._program)
             constants_string = self.constants.to_key_value_string(to_float=False) if self.constants else ""
             constant_definitions = description.parse_constant_definitions(constants_string)
-            self.program = description.instantiate_constants(constant_definitions)
+            self._program = description.instantiate_constants(constant_definitions)
 
             if description.is_prism_program:
-                self.program = self.program.as_prism_program()
+                self._program = self._program.as_prism_program()
             elif description.is_jani_model:
-                self.program = self.program.as_jani_model()
+                self._program = self._program.as_jani_model()
             else:
                 raise RuntimeError("Symbolic description type not known")
 
-        if not self.program.undefined_constants_are_graph_preserving:
-            raise RuntimeError("Given model file still contains undefined constants")
-
-        if self.prismfile.nr_parameters() == 0:
-            assert not self.program.has_undefined_constants
-            self.model = stormpy.build_model(self.program, self.pctlformula)
+    def set_bisimulation_type(self, bisimulation_type):
+        if bisimulation_type == BisimulationType.weak:
+            self.bisimulation = stormpy.BisimulationType.WEAK
+        elif bisimulation_type == BisimulationType.strong:
+            self.bisimulation = stormpy.BisimulationType.STRONG
+        elif bisimulation_type == BisimulationType.none:
+            self.bisimulation = None
         else:
-            assert self.program.has_undefined_constants
-            self.model = stormpy.build_parametric_model(self.program, self.pctlformula)
+            raise ConfigurationError("Bisimulation type {} not valid.".format(bisimulation_type))
 
-        if self.bisimulation == stormpy.BisimulationType.STRONG or self.bisimulation == stormpy.BisimulationType.WEAK:
-            logger.info("Perform bisimulation")
-            self.model = stormpy.perform_bisimulation(self.model, self.pctlformula, self.bisimulation)
-
-    def create_parameter_mapping(self, prophesy_parameters):
+    def get_model(self):
         """
-        Create a mapping from prophesy parameters to model parameters in stormpy.
+        Get model.
+        If model is not built before, first build the model and (optionally) perform bisimulation.
+        :return: Model.
+        """
+        if self._model is None:
+            logger.info("Build model")
+
+            if self.prismfile is None:
+                raise NotEnoughInformationError("Prism model not set.")
+            if self.pctlformula is None:
+                raise NotEnoughInformationError("PCTL formula not set.")
+            if not self._program.undefined_constants_are_graph_preserving:
+                raise RuntimeError("Given model file still contains undefined constants")
+
+            if self.prismfile.nr_parameters() == 0:
+                assert not self._program.has_undefined_constants
+                self._model = stormpy.build_model(self._program, self.pctlformula)
+            else:
+                assert self._program.has_undefined_constants
+                self._model = stormpy.build_parametric_model(self._program, self.pctlformula)
+
+            if self.bisimulation == stormpy.BisimulationType.STRONG or self.bisimulation == stormpy.BisimulationType.WEAK:
+                logger.info("Perform bisimulation")
+                self._model = stormpy.perform_bisimulation(self._model, self.pctlformula, self.bisimulation)
+        return self._model
+
+    def get_parameter_mapping(self, prophesy_parameters):
+        """
+        Get a mapping from prophesy parameters to model parameters in stormpy.
         :param prophesy_parameters: Parameters in prophesy.
-        :return: Dict of prophesy parameters to stormpy parameters.
+        :return: Mapping from prophesy parameters to model parameters.
         """
+        if self._parameter_mapping is None:
+            self._parameter_mapping = {}
+            model_parameters = self.get_model().collect_probability_parameters()
+            for parameter in prophesy_parameters:
+                param_string = str(parameter.variable)
+                model_param = next((var for var in model_parameters if str(var) == param_string), None)
+                assert model_param is not None
+                self._parameter_mapping[parameter] = model_param
 
-        assert self.model is not None
-        parameter_mapping = {}
-        model_parameters = self.model.collect_probability_parameters()
         for parameter in prophesy_parameters:
-            param_string = str(parameter.variable)
-            model_param = next((var for var in model_parameters if str(var) == param_string), None)
-            assert model_param is not None
-            parameter_mapping[parameter] = model_param
-        return parameter_mapping
+            assert parameter in self._parameter_mapping
+        return self._parameter_mapping
 
-    def check_model(self, model, property):
+    @staticmethod
+    def check_model(model, property):
         """
         Compute result of model checking.
         :param model: Model.
@@ -139,36 +155,50 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
         return pc.convert_from(result)
 
     def get_parameter_constraints(self):
-        if self.model is None:
-            self.build_model()
-
-        if self.parameter_constraints is None or self.graph_preservation_constraints is None:
+        if self._parameter_constraints is None or self._graph_preservation_constraints is None:
             # Collect constraints if not already there
             logger.info("Call stormpy for constraints")
 
-            collector = stormpy.ConstraintCollector(self.model)
-            self.parameter_constraints = []
-            self.graph_preservation_constraints = []
+            collector = stormpy.ConstraintCollector(self.get_model())
+            self._parameter_constraints = []
+            self._graph_preservation_constraints = []
             # Convert formulas to gmp
             for formula in collector.wellformed_constraints:
                 assert formula.type == pc.FormulaType.CONSTRAINT
                 converted_formula = pc.Formula(pc.convert_from(formula.get_constraint()))
-                self.parameter_constraints.append(converted_formula)
+                self._parameter_constraints.append(converted_formula)
             for formula in collector.graph_preserving_constraints:
                 assert formula.type == pc.FormulaType.CONSTRAINT
                 converted_formula = pc.Formula(pc.convert_from(formula.get_constraint()))
-                self.graph_preservation_constraints.append(converted_formula)
+                self._graph_preservation_constraints.append(converted_formula)
             logger.info("Stormpy call finished")
 
-        return self.parameter_constraints, self.graph_preservation_constraints
+        return self._parameter_constraints, self._graph_preservation_constraints
+
+    def get_model_instantiator(self):
+        """
+        Return model instantiator.
+        :return: Model instantiator.
+        """
+        if self._model_instantiator is None:
+            self._model_instantiator = stormpy.pars.PDtmcInstantiator(self.get_model())
+        return self._model_instantiator
+
+    def get_pla_checker(self, formula):
+        """
+        Return model checker for PLA.
+        :param formula: Formula to check.
+        :return: PLA model checker.
+        """
+        if self._pla_checker is None or self._pla_formula != formula:
+            self._pla_formula = formula
+            self._pla_checker = stormpy.pars.create_region_checker(self.get_model(), formula)
+        return self._pla_checker
 
     def get_rational_function(self):
-        if self.model is None:
-            self.build_model()
-
         # Compute rational function
         logger.info("Compute solution function")
-        rational_function = self.check_model(self.model, self.pctlformula[0])
+        rational_function = StormpyModelChecker.check_model(self.get_model(), self.pctlformula[0])
         logger.info("Stormpy model checking finished successfully")
 
         # Collect constraints
@@ -178,23 +208,16 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
                                 rational_function)
 
     def perform_sampling(self, samplepoints):
-        if self.model is None:
-            self.build_model()
-
-        logger.info("Perform uniform sampling")
-
-        # Initialize mapping from prophesy variables to model parameters in stormpy
-        parameter_mapping = self.create_parameter_mapping(samplepoints.parameters)
-
         # Perform sampling with model instantiator
         logger.info("Call stormpy for sampling")
         samples = InstantiationResultDict(samplepoints.parameters)
-        instantiator = stormpy.pars.PDtmcInstantiator(self.model)
-
+        parameter_mapping = self.get_parameter_mapping(samplepoints.parameters)
+        model_instantiator = self.get_model_instantiator()
         for sample_point in samplepoints:
+            # Instantiate point and check result
             point = {parameter_mapping[parameter]: pc.convert_to(val) for parameter, val in sample_point.items()}
-            instantiated_model = instantiator.instantiate(point)
-            result = self.check_model(instantiated_model, self.pctlformula[0])
+            instantiated_model = model_instantiator.instantiate(point)
+            result = StormpyModelChecker.check_model(instantiated_model, self.pctlformula[0])
             samples.add_result(InstantiationResult(sample_point, result))
 
         logger.info("Sampling with stormpy successfully finished")
@@ -202,9 +225,6 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
 
     def check_hyperrectangle(self, parameters, hyperrectangle, threshold, above_threshold):
         logger.info("Check region via stormpy")
-
-        if self.model is None:
-            self.build_model()
 
         # Set threshold and bound in formula
         formula = self.pctlformula[0].raw_formula
@@ -214,8 +234,8 @@ class StormpyModelChecker(ParametricProbabilisticModelChecker):
                           expression)
 
         # Initialize PLA checker
-        pla_checker = stormpy.pars.create_region_checker(self.model, formula)
-        model_parameters = self.model.collect_probability_parameters()
+        pla_checker = self.get_pla_checker(formula)
+        model_parameters = self.get_model().collect_probability_parameters()
         # Set region
         region_string = hyperrectangle.to_region_string(parameters.get_variables())
         logger.debug("Region string is {}".format(region_string))
