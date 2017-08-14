3a2b3e6796ccc3de34760c89b346fbe2b46f43be
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-08-14 15:47:20 +0200

    Added general files for examples

--- benchmarkfiles/nand/nand.pm
+++ benchmarkfiles/nand/nand.pm
@@ -5,8 +5,8 @@
 
 dtmc
 
-const int N; // number of inputs in each bundle
-const int K; // number of restorative stages
+const int N = 20; // number of inputs in each bundle
+const int K = 5; // number of restorative stages
 
 const int M = 2*K+1; // total number of multiplexing units
 
