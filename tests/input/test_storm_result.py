f4be2f5b66d6bc0f891e1da530fc70e687b93c9b
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-08-14 17:11:43 +0200

    Prefix test_ for tests

--- tests/input/test_storm_result.py
+++ tests/input/test_storm_result.py
@@ -4,4 +4,4 @@ from helpers.helper import get_example_path
 
 
 def test_read_pstorm_result_brp():
-    read_pstorm_result(get_example_path("examples", "brp", "brp_16-2.rat"))
+    read_pstorm_result(get_example_path("brp", "brp_16-2.rat"))
