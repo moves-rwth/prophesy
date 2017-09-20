3d55f67c25faa5e9f58621d486f7b62e8e06a321
Tom Janson <tom.janson@rwth-aachen.de>
2017-09-20 02:17:00 +0200

    parse cost function from CLI (using pycarl)

--- scripts/modelrepair.py
+++ scripts/modelrepair.py
@@ -23,8 +23,6 @@ import click
 import prophesy.adapter.pycarl as pc
 from prophesy.config import configuration
 from prophesy.data.constant import parse_constants_string
-from prophesy.data.point import Point
-from prophesy.data.samples import ParameterInstantiation
 from prophesy.input.pctlfile import PctlFile
 from prophesy.input.prismfile import PrismFile
 from prophesy.modelcheckers.prism import PrismModelChecker
@@ -62,12 +60,31 @@ def parse_parameters(prism_file, constants):
     return parameters
 
 
+# http://click.pocoo.org/5/parameters/#implementing-custom-types
+class PolynomialParamType(click.ParamType):
+    """Pycarl Polynomial CLI argument type for Click."""
+
+    name = 'polynomial'
+
+    def convert(self, value, param, ctx):
+        """Try to parse with pycarl.parse()."""
+        if value is None:
+            return None
+        try:
+            return pc.parse(value)
+        except pc.ParserError:
+            self.fail('%s cannot be parsed by pycarl' % value, param, ctx)
+
+
+POLYNOMIAL_TYPE = PolynomialParamType()
+
+
 # TODO:
-# * cost function (as string? as parsed by sympy or something?)
-#    * pycarl parser (prefix notation, see SMT lib solutionfunctionfile)
 # * what's needed to handle MDPs?
 # * hint / subsys analysis
-# * publish optim
+# * cost fct:
+#     * ask how to input exponents
+#     * ask if the unpacking can be handled gracefully
 
 @click.command()
 @click.option('--prism-file', help='parametric Markov chain in Prism file format', type=click.Path(exists=True),
@@ -75,17 +92,24 @@ def parse_parameters(prism_file, constants):
 @click.option('--pctl-file', help='PCTL property file containing property (e.g., P<=0.95 [F "target"])',
               type=click.Path(exists=True), default='../benchmarkfiles/brp/property_bound.pctl', required=True)
 @click.option('--pctl-index', help='index (0-based) of property in PCTL file', default=0, show_default=True)
+@click.option('--cost-function', help='polynomial cost function over the model\'s parameters (pycarl prefix notation)',
+              type=POLYNOMIAL_TYPE, default='(+ (* (- pK 0.6) (- pK 0.6)) (* (- pL 0.7) (- pL 0.7)))', required=False)
 @click.option('--modelchecker', type=click.Choice(MC_NAME_OPTIONS), default='stormpy', show_default=True)
 @click.option('--constants', help='additional constants string for the MC (rarely needed)', required=False)
-def model_repair(prism_file, pctl_file, pctl_index, modelchecker, constants):
+def model_repair(prism_file, pctl_file, pctl_index, cost_function, modelchecker, constants):
     """Find low-cost parameter valuation satisfying the PCTL property.
 
     Given a parametric model and a PCTL property, a heuristic search
     tries to find the lowest-cost parameter valuation for which the
     property is satisfied.
 
-    The cost function is currently hardcoded (i.e., not a command-line
-    option)."""
+    The cost function must be parseable by pycarl's parser. If it is
+    omitted, the cost is not considered [but then this procedure makes
+    little sense].
+
+    For demo purposes, the defaults currently show an example invocation
+    (rather than being "sensible" for general usage).
+    """
     prism_file = PrismFile(prism_file)
     mc = _get_selected_pmc(modelchecker)
     mc.load_model_from_prismfile(prism_file)
@@ -95,13 +119,13 @@ def model_repair(prism_file, pctl_file, pctl_index, modelchecker, constants):
 
     pctl_property = PctlFile(pctl_file).get(pctl_index)
 
-    def cost_function(parameter_instantiation):
-        """Return a cost (to be minimized) for a parameter valuation."""
-        # distance from some point, chosen just for demonstration
-        origin = ParameterInstantiation.from_point(Point(0.6, 0.7), parameters)
-        return origin.numerical_distance(parameter_instantiation)
+    if cost_function is None:
+        cost_function = pc.Polynomial(pc.parse("0"))
 
-    repairer = ModelRepairer(mc, parameters, pctl_property, cost_function)
+    # the unpacking in the cost fct is a bit awkward but required, because Parameter != Variable
+    repairer = ModelRepairer(mc, parameters, pctl_property,
+                             cost_fct=lambda instantiation: cost_function.evaluate({p.variable: v for p, v in
+                                                                                    instantiation.items()}))
     repairer.repair()
 
 
