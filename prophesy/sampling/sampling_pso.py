3a0b7203dff9d996fd8a776fbf06e52d9bf5b432
Tom Janson <tom.janson@rwth-aachen.de>
2017-09-25 03:33:09 +0200

    handle hint in ParticleSwarmSampleGenerator

--- prophesy/sampling/sampling_pso.py
+++ prophesy/sampling/sampling_pso.py
@@ -1,5 +1,6 @@
 import prophesy.adapter.pycarl as pc
 from heuristic_optimization.optimizers import ParticleSwarmOptimizer
+from heuristic_optimization.util.position_initializers import clamped_gaussian_distribution
 
 from prophesy.data.point import Point
 from prophesy.data.samples import ParameterInstantiations
@@ -10,10 +11,18 @@ def coords_to_rational_point(coords):
     return Point(*(pc.Rational(component) for component in coords))
 
 
+class GuidedParticleSwarmOptimizer(ParticleSwarmOptimizer):
+    def _generate_initial_positions(self):
+        return clamped_gaussian_distribution(self.options['num_particles'], mean_point=self.options['hint'],
+                                             bounds=(self.lower_bound, self.upper_bound),
+                                             standard_deviation=0.1)  # FIXME: check if that number is reasonable
+        # ... actually that should depend on the size of the search space, so maybe it should actually be based on that
+
+
 class ParticleSwarmSampleGenerator(SampleGenerator):
     """Perform PSO yielding each iterations' samples (particle positions)."""
 
-    def __init__(self, sampler, parameters, score_fct, pso_options=None):
+    def __init__(self, sampler, parameters, score_fct, hint=None, pso_options=None):
         super().__init__(sampler, parameters)
         self.score_fct = score_fct
         self.latest_sampling_result = None
@@ -25,7 +34,11 @@ class ParticleSwarmSampleGenerator(SampleGenerator):
 
         if pso_options is None:
             pso_options = {'num_particles': 20, 'max_iters': 20}
-        self.pso = ParticleSwarmOptimizer(self._objective, self.bounds, obj_fct_is_vectorized=True, options=pso_options)
+        pso_options['hint'] = hint
+
+        PSO = GuidedParticleSwarmOptimizer if hint is not None else ParticleSwarmOptimizer
+
+        self.pso = PSO(self._objective, self.bounds, obj_fct_is_vectorized=True, options=pso_options)
         self.pso.initialize()
 
     def _objective(self, list_of_coords):
