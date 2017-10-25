9dca9f790ed11659c4664b936a6cbf9c0482aadc
Tom Janson <tom.janson@rwth-aachen.de>
2017-10-25 20:04:59 +0200

    fix spelling

--- tests/data/test_range.py
+++ tests/data/test_range.py
@@ -1,8 +1,8 @@
-from prophesy.data.range import Range, create_cartisean_product
+from prophesy.data.range import Range, create_cartesian_product
 
 
 def test_cartesian_product():
     r1 = Range(1, 3, 1)
     r2 = Range(4, 6, 1)
-    result = create_cartisean_product([r1, r2])
+    result = create_cartesian_product([r1, r2])
     assert len(list(result)) == 9
